1eab1a98e48f737808213f8bd6ef4c5069f409f1
milerius <roman.sztergbaum@epitech.eu>
2018-12-21 22:38:02 +0100

    reformat according to coding style

--- service/protocol.hpp
+++ service/protocol.hpp
@@ -9,40 +9,40 @@
 
 namespace raven
 {
-    namespace json = nlohmann;
+  namespace json = nlohmann;
 
-    //! Keywords
-    static inline constexpr const char *request_keyword = "REQUEST_NAME";
+  //! Keywords
+  static inline constexpr const char *request_keyword = "REQUEST_NAME";
 
-    //! Protocol Constants
-    static inline constexpr const char *config_name_keyword = "CONFIG_NAME";
-    static inline constexpr const char *config_key_keyword = "CONFIG_KEY";
-    static inline constexpr const char *config_read_only_key_keyword = "READONLY_CONFIG_KEY";
+  //! Protocol Constants
+  static inline constexpr const char *config_name_keyword = "CONFIG_NAME";
+  static inline constexpr const char *config_key_keyword = "CONFIG_KEY";
+  static inline constexpr const char *config_read_only_key_keyword = "READONLY_CONFIG_KEY";
 
-    //! CONFIG_CREATE
-    struct config_create
-    {
-        std::string config_name;
-    };
+  //! CONFIG_CREATE
+  struct config_create
+  {
+    std::string config_name;
+  };
 
-    inline void from_json(const raven::json::json &json_data, config_create &cfg)
-    {
-        cfg.config_name = json_data.at(config_name_keyword).get<std::string>();
-    }
+  inline void from_json(const raven::json::json &json_data, config_create &cfg)
+  {
+      cfg.config_name = json_data.at(config_name_keyword).get<std::string>();
+  }
 
-    //! CONFIG_LOAD
-    struct config_load
-    {
-        std::optional<std::string> config_key{std::nullopt};
-        std::optional<std::string> config_read_only_key{std::nullopt};
-    };
+  //! CONFIG_LOAD
+  struct config_load
+  {
+    std::optional<std::string> config_key{std::nullopt};
+    std::optional<std::string> config_read_only_key{std::nullopt};
+  };
 
-    inline void from_json(const raven::json::json &json_data, config_load &cfg)
-    {
-        if (json_data.count(config_key_keyword) > 0) {
-            cfg.config_key = json_data.at(config_key_keyword).get<std::string>();
-        } else if (json_data.count(config_read_only_key_keyword) > 0) {
-            cfg.config_read_only_key = json_data.at(config_read_only_key_keyword).get<std::string>();
-        }
-    }
+  inline void from_json(const raven::json::json &json_data, config_load &cfg)
+  {
+      if (json_data.count(config_key_keyword) > 0) {
+          cfg.config_key = json_data.at(config_key_keyword).get<std::string>();
+      } else if (json_data.count(config_read_only_key_keyword) > 0) {
+          cfg.config_read_only_key = json_data.at(config_read_only_key_keyword).get<std::string>();
+      }
+  }
 }
