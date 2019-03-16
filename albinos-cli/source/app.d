db4474b21aab8b4d0a41f55b9cf863fbe4a50d7f
milerius <roman.sztergbaum@epitech.eu>
2019-03-16 21:27:27 +0100

    add class CLI

--- albinos-cli/source/app.d
+++ albinos-cli/source/app.d
@@ -18,20 +18,31 @@ class Client
 	private Socket socket_;
 }
 
+///
+class CLI 
+{
+	///
+	public void run_program()
+	{	
+		write("> ");
+		string line;
+		while ((line = stdin.readln().stripRight("\n")) !is null) {	
+			if (line == "exit")
+				break;
+			auto splited_line = line.split!isWhite;
+			if (splited_line.length >= 2) {
+				auto args = splited_line[1..splited_line.length];
+				auto cmd = splited_line[0];
+				writeln("cmd -> [", cmd, "]");
+				writeln("args -> ", args);
+			}
+			write("> ");
+		}
+	}
+}
+
 void main()
 {
-	write("> ");
-	string line;
-	while ((line = stdin.readln().stripRight("\n")) !is null) {	
-		if (line == "exit")
-			break;
-		auto splited_line = line.split!isWhite;
-		if (splited_line.length >= 2) {
-			auto args = splited_line[1..splited_line.length];
-			auto cmd = splited_line[0];
-			writeln("cmd -> [", cmd, "]");
-			writeln("args -> ", args);
-		}
-		write("> ");
-	}
+	auto cli = new CLI();
+	cli.run_program;
 }
