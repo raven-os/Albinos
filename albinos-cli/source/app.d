7734aead2569788c446dbad3d19d5927eb7fc2e2
milerius <roman.sztergbaum@epitech.eu>
2019-03-16 20:59:08 +0100

    update command line interpreter

--- albinos-cli/source/app.d
+++ albinos-cli/source/app.d
@@ -1,13 +1,21 @@
 import std.stdio;
+import std.string;
 
 void main()
 {
+	import std.uni : isWhite;
 	write("> ");
 	string line;
-	while ((line = stdin.readln()) !is null) {
-		if (line == "exit\n")
+	while ((line = stdin.readln().stripRight("\n")) !is null) {	
+		if (line == "exit")
 			break;
-		write(line);
+		auto splited_line = line.split!isWhite;
+		if (splited_line.length >= 2) {
+			auto args = splited_line[1..splited_line.length];
+			auto cmd = splited_line[0];
+			writeln("cmd -> [", cmd, "]");
+			writeln("args -> ", args);
+		}
 		write("> ");
 	}
 }
