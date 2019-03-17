fa0f23c4008815a45720db19a84bdd1e285f8254
milerius <roman.sztergbaum@epitech.eu>
2019-03-17 20:06:12 +0100

    create_config done :D

--- albinos-cli/source/app.d
+++ albinos-cli/source/app.d
@@ -1,8 +1,12 @@
 import std.stdio;
 import std.string;
+import std.getopt;
+import core.exception : AssertError;
 import std.socket : UnixAddress, Socket, SocketType, AddressFamily, SocketOSException;
 import std.uni : isWhite;
 import std.process : executeShell;
+import std.file;
+import asdf;
 
 ///
 class Client
@@ -13,6 +17,11 @@ class Client
 		this.address_ = new UnixAddress(socket_path);
 		this.socket_ = new Socket(AddressFamily.UNIX, SocketType.STREAM);
 		socket_.connect(this.address_);
+	}
+
+	@property Socket socket() pure nothrow @nogc
+	{
+		return socket_;
 	}
 
 	private UnixAddress address_;
@@ -34,6 +43,33 @@ class CLI
 			is_running_ = false;
 		}
 	}
+
+	private void create_config(string[] args)
+	in
+	{
+		assert(args !is null, "args cannot be null");
+		assert(args.length == 2, "need 1 arguments");
+	}
+	body
+	{
+		string config_name;
+		getopt(args, "name", &config_name);
+		struct config_create
+		{
+			@serializationKeys("REQUEST_NAME") string request_name;
+			@serializationKeys("CONFIG_NAME") string config_name;
+		}
+
+		auto cfg = (cast(const char[])(std.file.read("../API_doc/json_recipes/config_create.json")))
+			.deserialize!config_create;
+		cfg.config_name = config_name.strip("\"");
+		client_.socket.send(cfg.serializeToJson);
+		auto answer = new ubyte[256];
+		client_.socket.receive(answer);
+		client_.socket.getErrorText.writeln;
+		writeln(cast(string)answer);
+	}
+
 	///
 	public void run_program()
 	{
@@ -49,17 +85,21 @@ class CLI
 			}
 			auto splited_line = line.split!isWhite;
 			const auto cmd = splited_line[0];
-			string[] args = null;
-			if (splited_line.length >= 2)
+			try
 			{
-				args = splited_line[1 .. splited_line.length];
-				writeln("cmd -> [", cmd, "] args -> ", args);
+				switch (cmd)
+				{
+				case "create_config":
+					this.create_config(splited_line);
+					break;
+				default:
+					line.dup.executeShell.output.write;
+					break;
+				}
 			}
-			switch (cmd)
+			catch (AssertError e)
 			{
-			default:
-				line.dup.executeShell.output.write;
-				break;
+				writeln(stderr, e.msg);
 			}
 			write("> ");
 		}
@@ -69,8 +109,9 @@ class CLI
 	private bool is_running_ = true;
 }
 
-void main()
+void main(string[] args)
 {
+	writeln(args);
 	auto cli = new CLI("/tmp/raven-os_service_albinos.sock");
 	cli.run_program;
 }
