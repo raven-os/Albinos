aa0ec9a37b300725cffa8d3f70bff6ca3d73de02
Roman Sztergbaum <rmscastle@gmail.com>
2019-05-10 23:49:32 +0200

    feat(nim-editor): add history max len

--- albinos-editor/src/albinospkg/cli.nim
+++ albinos-editor/src/albinospkg/cli.nim
@@ -2,11 +2,15 @@ import terminal
 import rdstdin
 import linenoise
 
+func initCLI() {.inline.} =
+    discard historySetMaxLen(500)
+
 proc launchCLI*() =
-  while true:
-    var line: TaintedString
-    let res = readLineFromStdin("> ", line)
-    if res == false: continue
-    if line.string == "exit": break
-    if line.string == "clear":
-      clearScreen()
+    initCLI()
+    while true:
+        var line: TaintedString
+        let res = readLineFromStdin("> ", line)
+        if res == false: continue
+        if line.string == "exit": break
+        if line.string == "clear":
+            clearScreen()
