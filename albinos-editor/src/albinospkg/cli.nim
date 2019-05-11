5413f90482e2bb0baed3619048cd4dbb9b9bd080
Roman Sztergbaum <rmscastle@gmail.com>
2019-05-11 08:07:26 +0200

    use open array for specific cmd

--- albinos-editor/src/albinospkg/cli.nim
+++ albinos-editor/src/albinospkg/cli.nim
@@ -1,16 +1,42 @@
 import terminal
+import strutils
 import rdstdin
 import linenoise
 
-func initCLI() {.inline.} =
-    discard historySetMaxLen(500)
+let doc = """
+Usage:
+    help (clear the screen)
+    exit (quit the cli)
+    clear (clear the terminal)
+    config create <name> (create a config with the given name)
+    config load <key> (load a config with the given key)
+
+Example:
+    > config create my_cfg
+"""
+
+proc handleConfigCmd(argsSet: openArray[string]) =
+    echo argsSet
 
 proc launchCLI*() =
-    initCLI()
+    discard historySetMaxLen(500)
     while true:
         var line: TaintedString
         let res = readLineFromStdin("> ", line)
-        if res == false: continue
-        if line.string == "exit": break
-        if line.string == "clear":
-            clearScreen()
+        if res == false:
+            styledEcho(fgMagenta, "readline error, quitting CLI mode...")
+            break
+        let args = line.string.unindent.splitWhitespace
+        case args[0]:
+            of "exit": break
+            of "help":
+                echo doc
+                continue
+            of "clear":
+                clearScreen()
+                continue
+            of "config":
+                handleConfigCmd(toOpenArray(args, 0, len(args) - 1))
+            else:
+                echo doc
+
